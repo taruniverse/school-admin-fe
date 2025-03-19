04ede845692febbd066b9f36445b06702b77c6b1
Tarun K. Gupta <tg.github@gmail.com>
2025-03-19 23:26:57 +0530

    alert and not found component added

---
+++ src/app/common/not-found-data/not-found-data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found-data',
+  imports: [],
+  templateUrl: './not-found-data.component.html',
+  styleUrl: './not-found-data.component.scss'
+})
+export class NotFoundDataComponent {
+
+}
--- src/app/main/student/student.component.ts
+++ src/app/main/student/student.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ModalComponent } from '../../common/modal/modal.component';
+import { NotFoundDataComponent } from '../../common/not-found-data/not-found-data.component';
 
 @Component({
   selector: 'app-student',
-  imports: [],
+  imports: [NotFoundDataComponent],
   templateUrl: './student.component.html',
   styleUrl: './student.component.scss'
 })
